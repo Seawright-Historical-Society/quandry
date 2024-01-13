9a9f4030494bd6959abbfe0ff8602217a49891e1
Arnab Ghosh <arnabcare21@gmail.com>
2024-01-13 14:36:16 -0800

    Add NextUI (Dashboard UI components)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 import { type AppType } from 'next/app';
 
 import { api } from '~/utils/api';
+import {NextUIProvider} from "@nextui-org/react";
 
 import '~/styles/globals.css';
 
@@ -12,7 +13,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
 	return (
 		<SessionProvider session={session}>
-			<Component {...pageProps} />
+			<NextUIProvider>
+				<Component {...pageProps} />
+			</NextUIProvider>
 		</SessionProvider>
 	);
 };
